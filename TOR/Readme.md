25dc00ddad4a94d549fa057d04eb516babd86565
Jon Herrmann <herrmann@interactive-instruments.de>
2018-06-20 12:02:48 +0200

    Moved from docs repository

---
+++ TOR/Readme.md
@@ -0,0 +1,4 @@
+# Terms of reference
+
+* [Steering Group](Steering_Group.adoc)
+* [Technical Committee](Technical_Committee.adoc)
---
+++ TOR/convert_docs.sh
@@ -0,0 +1,7 @@
+#/bin/bash
+
+# Prerequisite: gem install asciidoctor && gem install pygments.rb
+
+asciidoctor \
+Steering_Group.adoc \
+Technical_Committee.adoc
